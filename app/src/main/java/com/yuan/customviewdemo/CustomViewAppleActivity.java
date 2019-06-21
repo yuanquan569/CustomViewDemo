ef9501e6bb3c1568397aad5f1e42071420f3d534
xhfHIT <3529203702@qq.com>
2019-06-22 00:55:51 +0800

    Initial commit

---
+++ app/src/main/java/com/yuan/customviewdemo/CustomViewAppleActivity.java
@@ -0,0 +1,14 @@
+package com.yuan.customviewdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CustomViewAppleActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apple);
+    }
+}
